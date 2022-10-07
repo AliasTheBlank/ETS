247920016900e8bc837be9ff7c451732f76a8202
AliasTheBlank <aliastheblank@gmail.com>
2022-10-07 19:09:51 -0400

    feat: sponsor class created

--- ETS.Library/ETS.Library/Sponsor.cs
+++ ETS.Library/ETS.Library/Sponsor.cs
@@ -8,8 +8,36 @@ namespace ETS.Library
 {
     class Sponsor : Person
     {
+        private string _sponsorID;
+        private double _totalPrizeValue;
+
         public Sponsor(string firstName, string lastName) : base(firstName, lastName)
         {
+        }
+
+        public Sponsor(string firstName, string lastName, string sponsorID, double totalPrizeValue) : this(firstName, lastName)
+        {
+            _sponsorID = sponsorID;
+            _totalPrizeValue = totalPrizeValue;
+        }
+
+        public string SponsorID { get => _sponsorID; set => _sponsorID = value; }
+        public double TotalPrizeValue { get => _totalPrizeValue; set => _totalPrizeValue = value; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $",{SponsorID},{TotalPrizeValue}";
+        }
+
+        public string GetID()
+        {
+            return SponsorID;
+        }
+
+        public double AddValue(double valueToAdd)
+        {
+            _totalPrizeValue += valueToAdd;
+            return TotalPrizeValue;
         }
     }
 }
