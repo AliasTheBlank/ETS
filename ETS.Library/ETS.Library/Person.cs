26e7f8dda2cedec06e3ebe35667fbcdc888ef2aa
AliasTheBlank <aliastheblank@gmail.com>
2022-10-07 18:59:41 -0400

    feat: person class created

---
+++ ETS.Library/ETS.Library/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS.Library
+{
+    public abstract class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+
+        public override string ToString()
+        {
+            return $"{_firstName},{_lastName}";
+        }
+    }
+}
---
+++ ETS.Library/ETS.Library/Sponsor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS.Library
+{
+    class Sponsor : Person
+    {
+        public Sponsor(string firstName, string lastName) : base(firstName, lastName)
+        {
+        }
+    }
+}
