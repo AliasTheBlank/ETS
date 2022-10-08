2068b13bc9c32837f70174332b3a785852c5aad4
AliasTheBlank <aliastheblank@gmail.com>
2022-10-07 21:18:48 -0400

    feat: interfaces for main classes were added

---
+++ ETS.Library/ETS.Library/Donors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS.Library
+{
+    internal class Donors : CollectionBase
+    {
+        public void Add(Donor newDonor)
+        {
+            List.Add(newDonor);
+        }
+
+        public Donor this[int index]
+        {
+            get { return (Donor)this[index]; }
+            set { this[index] = value; }
+        }
+    }
+}
---
+++ ETS.Library/ETS.Library/Prizes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS.Library
+{
+    internal class Prizes : CollectionBase
+    {
+        public void Add(Prize newPrize)
+        {
+            List.Add(newPrize);
+        }
+
+        public Prize this[int index]
+        {
+            get { return (Prize)this[index]; }
+            set { this[index] = value; }
+        }
+    }
+}
